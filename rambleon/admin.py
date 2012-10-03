2cb4d5e0ad08f115d4cbac26310aba6ee869ac21
Baxter <baxterj405@gmail.com>
2012-10-03 16:29:45 +0100

    add routes to admin bugfix 2

--- rambleon/admin.py
+++ rambleon/admin.py
@@ -6,10 +6,6 @@ class PointInline(admin.TabularInline):
 	extra = 3
 
 class RoutesAdmin(admin.ModelAdmin):
-	fieldsets = [
-		(None,               {'fields': ['name']}),
-		('Date information', {'fields': ['creation_date', 'update_date'], 'classes': ['collapse']}),
-	]
 	inlines = [PointInline]
 	list_display = ('name', 'creation_date', 'update_date')
 
