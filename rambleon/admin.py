12b9e51ff14d275b03ee12f3a9368905b40005fd
Baxter <baxterj405@gmail.com>
2012-10-03 16:24:27 +0100

    add routes to admin

---
+++ rambleon/admin.py
@@ -0,0 +1,16 @@
+from rambleon.models import Route, PathPoint
+from django.contrib import admin
+
+class PointInline(admin.TabularInline):
+	model = PathPoint
+	extra = 3
+
+class RoutesAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,               {'fields': ['name']}),
+		('Date information', {'fields': ['creation_date', 'update_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+	list_display = ('name', 'creation_date', 'update_date')
+
+admin.site.register(Route, RoutesAdmin)
