1937bcc0b686e82affc1b49889e18c1f95e97b4c
unknown <baxterj405@gmail.com>
2012-12-19 22:11:07 +0000

    format for datetimes, add done and fav to individual route info

--- rambleon/getHandlers.py
+++ rambleon/getHandlers.py
@@ -33,11 +33,35 @@ def dehydrateRoutesList(bundle):
 			words.append(i)
 		bundle.data['keywords'] = words
 
+	bundle.data['creation_date'] = bundle.data['creation_date'].strftime('%d %b %Y')
+	bundle.data['update_date'] = bundle.data['update_date'].strftime('%d %b %Y')
+
 	bundle.data.pop('resource_uri')
 
 	return bundle
 
 def dehydrateSingleRoute(bundle):
+
+	if bundle.obj.favourites.all().count() < 1:
+			bundle.data['fav'] = False
+	else:
+		for i in bundle.obj.favourites.all():
+			if i == User.objects.get(username__iexact=bundle.request.GET.get('user')):
+				bundle.data['fav'] = True
+				break
+			else:
+				bundle.data['fav'] = False
+
+	if bundle.obj.doneIts.all().count() < 1:
+		bundle.data['done'] = False
+	else:
+		for i in bundle.obj.doneIts.all():
+			if i == User.objects.get(username__iexact=bundle.request.GET.get('user')):
+				bundle.data['done'] = True
+				break
+			else:
+				bundle.data['done'] = False
+	
 	if bundle.obj.keywords.all().count() < 1:
 		bundle.data['keywords'] = False
 	else:
@@ -49,6 +73,9 @@ def dehydrateSingleRoute(bundle):
 
 	if bundle.obj.pathpoints.all().count() < 1:
 		bundle.data['pathpoints'] = False
+
+	bundle.data['creation_date'] = bundle.data['creation_date'].strftime('%d %b %Y')
+	bundle.data['update_date'] = bundle.data['update_date'].strftime('%d %b %Y')
 
 	bundle.data.pop('resource_uri')
 
